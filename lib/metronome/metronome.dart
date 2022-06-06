44f3467b3a86452dd235edafc3d6bc21c517dd25
BeMain <bemain.dev@gmail.com>
2022-06-06 14:13:56 +0200

    Create Metronome singleton

---
+++ lib/metronome/metronome.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class Metronome {
+  /// Beats per minutes.
+  ///
+  /// Automatically resets [count] when changed.
+  static int get bpm => bpmNotifier.value;
+  static set bpm(int value) => bpmNotifier.value = value;
+  static ValueNotifier<int> bpmNotifier = ValueNotifier(60)
+    ..addListener(restart);
+
+  /// Beats per bar.
+  ///
+  /// Automatically resets [count] when changed.
+  static int get higher => higherNotifier.value;
+  static set higher(int value) => higherNotifier.value = value;
+  static ValueNotifier<int> higherNotifier = ValueNotifier(4)
+    ..addListener(restart);
+
+  /// Current beat. Ranges from 0 to [higher] - 1.
+  static int get count => countNotifier.value;
+  static set count(int value) => countNotifier.value = value;
+  static ValueNotifier<int> countNotifier = ValueNotifier(0);
+
+  /// Whether or not the metronome is playing.
+  static bool get isRunning => isRunningNotifier.value;
+  static set isRunning(bool value) => isRunningNotifier.value = value;
+  static ValueNotifier<bool> isRunningNotifier = ValueNotifier(false);
+
+  /// Internal [Timer], calls [_onTimeout] [bpm] times per minute.
+  static Timer _timer = Timer(const Duration(), () {});
+
+  /// Called on [_timer] timeout.
+  /// Increases [count] and plays a sound.
+  static void _onTimeout(Timer timer) {
+    count++;
+    count %= higher;
+  }
+
+  /// Start the metronome.
+  static void start() {
+    if (!_timer.isActive) {
+      _timer = Timer.periodic(Duration(milliseconds: 60000 ~/ bpm), _onTimeout);
+      count = 0;
+      isRunning = true;
+    }
+  }
+
+  /// Stop the metronome.
+  static void stop() {
+    if (_timer.isActive) {
+      _timer.cancel();
+      isRunning = false;
+    }
+  }
+
+  /// Stop the metronome if it's running, then start it again.
+  static void restart() {
+    stop();
+    start();
+  }
+}
