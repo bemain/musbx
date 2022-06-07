ecffc1e109311f5fd5c5131f90c2bb4e59517ce6
BeMain <bemain.dev@gmail.com>
2022-06-07 20:45:29 +0200

    Add slider for adjusting bpm

--- lib/metronome/bottom_bar.dart
+++ lib/metronome/bottom_bar.dart
@@ -12,55 +12,100 @@ class MetronomeBottomBarState extends State<MetronomeBottomBar> {
   @override
   Widget build(BuildContext context) {
     return BottomAppBar(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          ValueListenableBuilder<bool>(
-            valueListenable: Metronome.isRunningNotifier,
-            builder: (context, bool isRunning, child) {
-              return TextButton(
-                onPressed: () {
-                  if (isRunning) {
-                    Metronome.stop();
-                  } else {
-                    Metronome.start();
-                  }
-                },
-                child: isRunning
-                    ? const Icon(Icons.stop)
-                    : const Icon(Icons.play_arrow),
-              );
-            },
-          ),
-          SizedBox(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ContinuousButton(
-                  onPressed: () {
-                    if (Metronome.bpm < Metronome.maxBpm) Metronome.bpm++;
-                  },
-                  child: const Icon(Icons.arrow_drop_up),
-                ),
-                ValueListenableBuilder(
-                  valueListenable: Metronome.bpmNotifier,
-                  builder: (BuildContext context, int bpm, Widget? child) =>
-                      Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Text("$bpm"),
-                  ),
-                ),
-                ContinuousButton(
-                  onPressed: () {
-                    if (Metronome.bpm > Metronome.minBpm) Metronome.bpm--;
-                  },
-                  child: const Icon(Icons.arrow_drop_down),
-                )
-              ],
+      child: SizedBox(
+        height: 100,
+        child: Row(
+          children: <Widget>[
+            _buildPlayButton(),
+            Expanded(
+              child: Column(
+                children: [
+                  _buildBpmSlider(),
+                  _buildBpmButtons(),
+                ],
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildPlayButton() {
+    return ValueListenableBuilder<bool>(
+      valueListenable: Metronome.isRunningNotifier,
+      builder: (context, bool isRunning, child) {
+        return TextButton(
+          onPressed: () {
+            if (isRunning) {
+              Metronome.stop();
+            } else {
+              Metronome.start();
+            }
+          },
+          child: Icon(
+            isRunning ? Icons.stop_rounded : Icons.play_arrow_rounded,
+            size: 75,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildBpmSlider() {
+    return ValueListenableBuilder(
+      valueListenable: Metronome.bpmNotifier,
+      builder: (c, int bpm, Widget? child) {
+        return Slider(
+          min: Metronome.minBpm.toDouble(),
+          max: Metronome.maxBpm.toDouble(),
+          value: Metronome.bpm.toDouble(),
+          onChanged: (double value) {
+            Metronome.bpm = value.toInt();
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildBpmButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ContinuousButton(
+          onPressed: () {
+            if (Metronome.bpm < Metronome.maxBpm) Metronome.bpm++;
+          },
+          child: const Icon(
+            Icons.arrow_drop_up,
+            size: 35,
+          ),
+        ),
+        Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: _buildBpmText()),
+        ContinuousButton(
+          onPressed: () {
+            if (Metronome.bpm > Metronome.minBpm) Metronome.bpm--;
+          },
+          child: const Icon(
+            Icons.arrow_drop_down,
+            size: 35,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildBpmText() {
+    return ValueListenableBuilder(
+      valueListenable: Metronome.bpmNotifier,
+      builder: (c, int bpm, Widget? child) {
+        return Text(
+          "$bpm",
+          style: const TextStyle(fontSize: 20),
+        );
+      },
     );
   }
 }
