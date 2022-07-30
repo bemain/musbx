ad34f5c5727dd9736663b1a12bb96f1e4a8856e6
BeMain <bemain.dev@gmail.com>
2022-07-30 15:35:48 +0200

    Rename file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:musbx/metronome/bottom_bar.dart';
-import 'package:musbx/music_player/slowdowner_screen.dart';
+import 'package:musbx/music_player/music_player_screen.dart';
 
 void main() {
   runApp(const MyApp());
