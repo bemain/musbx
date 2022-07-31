8633cf1983ba712f4e6c52ace4c9bda8b02f954e
BeMain <bemain.dev@gmail.com>
2022-07-31 10:47:55 +0200

    Disable buttons if no song is loaded

--- lib/music_player/button_panel.dart
+++ lib/music_player/button_panel.dart
@@ -3,51 +3,66 @@ import 'package:musbx/music_player/music_player.dart';
 
 class ButtonPanel extends StatelessWidget {
   /// Panel including play/pause, forward and rewind buttons for controlling [MusicPlayer].
+  ///
+  /// If no song is loaded, all buttons are disabled.
   const ButtonPanel({super.key});
 
   @override
   Widget build(BuildContext context) {
     final MusicPlayer player = MusicPlayer.instance;
 
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        TextButton(
-          onPressed: () {
-            player.seek(player.position - const Duration(seconds: 1));
-          },
-          onLongPress: () {
-            player.seek(Duration.zero);
-          },
-          child: const Icon(Icons.fast_rewind_rounded, size: 40),
-        ),
-        StreamBuilder<bool>(
-          stream: player.playingStream,
-          initialData: false,
-          builder: (context, snapshot) {
-            bool isPlaying = snapshot.data!;
-            return TextButton(
-              onPressed: (() {
-                if (isPlaying) {
-                  player.pause();
-                } else {
-                  player.play();
-                }
-              }),
-              child: Icon(
-                isPlaying ? Icons.stop_rounded : Icons.play_arrow_rounded,
-                size: 75,
-              ),
-            );
-          },
-        ),
-        TextButton(
-          onPressed: () {
-            player.seek(player.position + const Duration(seconds: 1));
-          },
-          child: const Icon(Icons.fast_forward_rounded, size: 40),
-        ),
-      ],
+    return ValueListenableBuilder(
+      valueListenable: player.songTitleNotifier,
+      builder: (context, songTitle, child) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: (songTitle == null)
+                  ? null
+                  : () {
+                      player.seek(player.position - const Duration(seconds: 1));
+                    },
+              onLongPress: (songTitle == null)
+                  ? null
+                  : () {
+                      player.seek(Duration.zero);
+                    },
+              child: const Icon(Icons.fast_rewind_rounded, size: 40),
+            ),
+            StreamBuilder<bool>(
+              stream: player.playingStream,
+              initialData: false,
+              builder: (context, snapshot) {
+                bool isPlaying = snapshot.data!;
+                return TextButton(
+                  onPressed: (songTitle == null)
+                      ? null
+                      : () {
+                          if (isPlaying) {
+                            player.pause();
+                          } else {
+                            player.play();
+                          }
+                        },
+                  child: Icon(
+                    isPlaying ? Icons.stop_rounded : Icons.play_arrow_rounded,
+                    size: 75,
+                  ),
+                );
+              },
+            ),
+            TextButton(
+              onPressed: (songTitle == null)
+                  ? null
+                  : () {
+                      player.seek(player.position + const Duration(seconds: 1));
+                    },
+              child: const Icon(Icons.fast_forward_rounded, size: 40),
+            ),
+          ],
+        );
+      },
     );
   }
 }
