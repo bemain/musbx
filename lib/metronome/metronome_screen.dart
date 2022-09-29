131d9ed6c5a0d34bb8e7574de84328bdbaa6387f
BeMain <bemain.dev@gmail.com>
2022-09-29 18:28:01 +0200

    Use BpmButtons widget

--- lib/metronome/metronome_screen.dart
+++ lib/metronome/metronome_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:musbx/editable_screen/editable_screen.dart';
 import 'package:musbx/metronome/beat_sound_viewer.dart';
+import 'package:musbx/metronome/bottom_bar/bpm_buttons.dart';
 import 'package:musbx/metronome/bpm_slider.dart';
-import 'package:musbx/metronome/metronome.dart';
 import 'package:musbx/metronome/play_button.dart';
-import 'package:musbx/widgets.dart';
 
 class MetronomeScreen extends StatelessWidget {
   const MetronomeScreen({super.key});
@@ -22,46 +21,14 @@ class MetronomeScreen extends StatelessWidget {
           ),
           BeatSoundViewer(),
         ]),
-        Column(children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ContinuousButton(
-                onPressed: () {
-                  Metronome.bpm--;
-                },
-                child: const Icon(
-                  Icons.arrow_drop_up_rounded,
-                  size: 50,
-                ),
-              ),
-              _buildBpmText(context),
-              ContinuousButton(
-                onPressed: () {
-                  Metronome.bpm++;
-                },
-                child: const Icon(
-                  Icons.arrow_drop_down_rounded,
-                  size: 50,
-                ),
-              ),
-            ],
+        Column(children: const [
+          BpmButtons(
+            iconSize: 50,
+            fontSize: 45,
           ),
-          const BpmSlider(),
+          BpmSlider(),
         ]),
       ],
-    );
-  }
-
-  Widget _buildBpmText(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: Metronome.bpmNotifier,
-      builder: (c, int bpm, Widget? child) {
-        return Text(
-          "$bpm",
-          style: const TextStyle(fontSize: 45),
-        );
-      },
     );
   }
 }
