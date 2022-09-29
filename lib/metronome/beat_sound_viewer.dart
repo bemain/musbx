8bbf0237cb0f1567f52237c2b40e77643b5d9d70
BeMain <bemain.dev@gmail.com>
2022-09-29 18:08:49 +0200

    Highlight selected beat by filling it in

--- lib/metronome/beat_sound_viewer.dart
+++ lib/metronome/beat_sound_viewer.dart
@@ -72,11 +72,10 @@ class BeatSoundViewerState extends State<BeatSoundViewer> {
                   child: Padding(
                     padding: const EdgeInsets.all(10),
                     child: Icon(
-                      Icons.circle,
+                      (Metronome.count == index)
+                          ? Icons.circle
+                          : Icons.circle_outlined, // Highlight current beat
                       color: sound.color,
-                      shadows: (Metronome.count == index)
-                          ? [const Shadow(blurRadius: 10)]
-                          : [], // Highlight current beat
                     ),
                   ),
                 ),
