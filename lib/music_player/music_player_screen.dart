f3a1357745c1396ccb6b1f1f69c428b67abeb7a4
BeMain <bemain.dev@gmail.com>
2022-10-14 12:40:23 +0200

    Move speed and pitch slider into seperate widget

--- lib/music_player/music_player_screen.dart
+++ lib/music_player/music_player_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:musbx/editable_screen/card_list.dart';
+import 'package:musbx/music_player/pitch_speed_card.dart';
 import 'package:musbx/music_player/position_card/button_panel.dart';
 import 'package:musbx/music_player/current_song_card/current_song_label.dart';
-import 'package:musbx/music_player/labeled_slider.dart';
 import 'package:musbx/music_player/loop_card/loop_buttons.dart';
 import 'package:musbx/music_player/loop_card/loop_slider.dart';
-import 'package:musbx/music_player/music_player.dart';
 import 'package:musbx/music_player/position_card/position_slider.dart';
 
 class MusicPlayerScreen extends StatelessWidget {
@@ -18,27 +17,12 @@ class MusicPlayerScreen extends StatelessWidget {
   ///  - Label showing current song, and button to load a song from device.
   MusicPlayerScreen({super.key});
 
-  final MusicPlayer musicPlayer = MusicPlayer.instance;
-
   @override
   Widget build(BuildContext context) {
     return CardList(
       children: [
         const CurrentSongPanel(),
-        Column(
-          children: [
-            Text(
-              "Pitch",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            buildPitchSlider(),
-            Text(
-              "Speed",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            buildSpeedSlider(),
-          ],
-        ),
+        PitchSpeedCard(),
         Column(children: const [
           LoopButtons(),
           LoopSlider(),
@@ -50,58 +34,6 @@ class MusicPlayerScreen extends StatelessWidget {
           ],
         ),
       ],
-    );
-  }
-
-  Widget buildPitchSlider() {
-    return ValueListenableBuilder(
-      valueListenable: musicPlayer.songTitleNotifier,
-      builder: (_, songTitle, __) => ValueListenableBuilder(
-        valueListenable: musicPlayer.pitchSemitonesNotifier,
-        builder: (context, pitch, _) => LabeledSlider(
-          label: pitch.toStringAsFixed(1),
-          clearDisabled: pitch == 0,
-          onClear: () {
-            musicPlayer.setPitchSemitones(0);
-          },
-          child: Slider(
-            value: pitch,
-            min: -9,
-            max: 9,
-            divisions: 18,
-            label: pitch.toStringAsFixed(1),
-            onChanged: musicPlayer.nullIfNoSongElse(
-              musicPlayer.setPitchSemitones,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildSpeedSlider() {
-    return ValueListenableBuilder(
-      valueListenable: musicPlayer.songTitleNotifier,
-      builder: (_, songTitle, __) => ValueListenableBuilder(
-        valueListenable: musicPlayer.speedNotifier,
-        builder: (context, speed, _) => LabeledSlider(
-          label: speed.toStringAsFixed(1),
-          clearDisabled: speed.toStringAsFixed(2) == "1.00",
-          onClear: () {
-            musicPlayer.setSpeed(1.0);
-          },
-          child: Slider(
-            value: speed,
-            min: 0.1,
-            max: 1.9,
-            divisions: 18,
-            label: speed.toStringAsFixed(1),
-            onChanged: musicPlayer.nullIfNoSongElse(
-              musicPlayer.setSpeed,
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
