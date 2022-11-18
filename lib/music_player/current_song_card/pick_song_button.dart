f1742c7424499519b138b3bee0e6848e2b648cf4
BeMain <bemain.dev@gmail.com>
2022-11-18 17:16:13 +0100

    Only allow files with specific extensions
    Alerts the user if the selected file is of an unsupported type

--- lib/music_player/current_song_card/pick_song_button.dart
+++ lib/music_player/current_song_card/pick_song_button.dart
@@ -4,32 +4,45 @@ import 'package:musbx/music_player/music_player.dart';
 import 'package:musbx/permission_builder.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-class PickSongButton extends StatelessWidget {
-  /// Button for picking a song from the device and loading it to [MusicPlayer].
-  PickSongButton({super.key});
+const List<String> allowedExtensions = [
+  "mp3",
+  "ogg",
+  "wav",
+  "mp4",
+  "m4a",
+  "mka",
+];
 
+class PickSongButton extends StatelessWidget {
   /// Whether permission to read external storage has been given or not.
   static bool permissionGranted = false;
 
+  /// Button for picking a song from the device and loading it to [MusicPlayer].
+  PickSongButton({super.key});
+
   final MusicPlayer musicPlayer = MusicPlayer.instance;
+
+  /// Required to show dialog. Probably not the best way to do this...
+  late final BuildContext _navigatorContext;
 
   @override
   Widget build(BuildContext context) {
+    _navigatorContext = Navigator.of(context).context;
     return OutlinedButton(
       onPressed: musicPlayer.isLoading
           ? null
           : () {
               if (permissionGranted) {
-                pickFile();
-                return;
+                pickFile(context);
+              } else {
+                pushPermissionBuilder(context);
               }
-              pushPermissionBuilder(context);
             },
       child: const Icon(Icons.file_upload_rounded),
     );
   }
 
-  Future<void> pickFile() async {
+  Future<void> pickFile(BuildContext context) async {
     MusicPlayerState prevState = musicPlayer.state;
     musicPlayer.stateNotifier.value = MusicPlayerState.pickingAudio;
 
@@ -37,17 +50,51 @@ class PickSongButton extends StatelessWidget {
       type: FileType.audio,
     );
 
-    if (result != null && result.files.single.path != null) {
-      await musicPlayer.playFile(result.files.single);
-    } else {
+    if (result == null || result.files.single.path == null) {
       // Restore state
       musicPlayer.stateNotifier.value = prevState;
+      return;
     }
+    String extension = result.files.single.path!.split(".").last;
+    if (!allowedExtensions.contains(extension)) {
+      showUnsupportedFileExtensionDialog(extension);
+      // Restore state
+      musicPlayer.stateNotifier.value = prevState;
+      return;
+    }
+
+    await musicPlayer.playFile(result.files.single);
+  }
+
+  Future<void> showUnsupportedFileExtensionDialog(String extension) async {
+    await showDialog(
+      context: _navigatorContext,
+      builder: (context) => AlertDialog(
+        title: const Text("Unsupported file type"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Icon(Icons.file_present_rounded, size: 128),
+            const SizedBox(height: 15),
+            Text(
+                "The file type '.$extension' is not supported. Try loading a different file.")
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text("Dismiss"),
+          )
+        ],
+      ),
+    );
   }
 
   void pushPermissionBuilder(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) => Scaffold(
+      builder: (newContext) => Scaffold(
         body: PermissionBuilder(
           permission: Permission.storage,
           permissionName: "external storage",
@@ -58,8 +105,8 @@ class PickSongButton extends StatelessWidget {
           onPermissionGranted: () {
             permissionGranted = true;
 
-            Navigator.of(context).pop();
-            pickFile();
+            Navigator.of(newContext).pop();
+            pickFile(context);
           },
         ),
       ),
