3d83c652c8b41ed27d60b7f048b22aa61072d510
BeMain <bemain.dev@gmail.com>
2023-01-09 21:42:18 +0100

    Update documentation

--- lib/music_player/music_player_component.dart
+++ lib/music_player/music_player_component.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:musbx/music_player/music_player.dart';
 
 /// Base class for all components that alter the audio being played by [MusicPlayer].
+///
+/// The component is responsible for all communication between itself and MusicPlayer.
 abstract class MusicPlayerComponent {
   /// Whether we are currently looping a section of the song or not.
   bool get enabled => enabledNotifier.value;
   set enabled(bool value) => enabledNotifier.value = value;
   final ValueNotifier<bool> enabledNotifier = ValueNotifier(true);
 
-  /// Called when [MusicPlayer] is created.
+  /// Called by [MusicPlayer] during initialization.
   ///
-  /// During initialization, the component is responsible for setting up all the
+  /// When this method is called, the component should set up all the
   /// communication needed between itself and [MusicPlayer].
   /// This includes listening to any of [MusicPlayer]'s ValueNotifiers that the
   /// component needs to respond to, and adding triggers for updating values on [MusicPlayer].
