e96ae731536a18a16ca24ff1f98fb7fe6c26052d
BeMain <bemain.dev@gmail.com>
2023-02-13 20:53:22 +0100

    Create EqualizerOverlayPainter

--- lib/music_player/equalizer/equalizer_card.dart
+++ lib/music_player/equalizer/equalizer_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:musbx/music_player/equalizer/equalizer_overlay.dart';
 import 'package:musbx/music_player/music_player.dart';
 
 class EqualizerCard extends StatelessWidget {
@@ -14,89 +15,113 @@ class EqualizerCard extends StatelessWidget {
       builder: (context, parameters, child) => ValueListenableBuilder(
         valueListenable: musicPlayer.equalizer.enabledNotifier,
         builder: (context, equalizerEnabled, child) {
-          return Column(
-            children: [
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Switch(
-                      value: equalizerEnabled,
-                      onChanged: musicPlayer.nullIfNoSongElse(
-                        (value) => musicPlayer.equalizer.enabled = value,
-                      ),
+          return Column(children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Switch(
+                    value: equalizerEnabled,
+                    onChanged: musicPlayer.nullIfNoSongElse(
+                      (value) => musicPlayer.equalizer.enabled = value,
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Center(
-                      child: Text(
-                        "Equalizer",
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Center(
+                    child: Text(
+                      "Equalizer",
+                      style: Theme.of(context).textTheme.titleMedium,
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: IconButton(
-                      iconSize: 20,
-                      onPressed: musicPlayer.nullIfNoSongElse(() {
-                        if (parameters != null) {
-                          musicPlayer.equalizer.resetGain();
-                        }
-                      }),
-                      icon: const Icon(Icons.refresh_rounded),
-                    ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: IconButton(
+                    iconSize: 20,
+                    onPressed: musicPlayer.nullIfNoSongElse(() {
+                      if (parameters != null) {
+                        musicPlayer.equalizer.resetGain();
+                      }
+                    }),
+                    icon: const Icon(Icons.refresh_rounded),
                   ),
-                ],
-              ),
-              buildEqualizerSliders(equalizerEnabled, parameters: parameters),
-            ],
-          );
+                ),
+              ],
+            ),
+            buildEqualizerControls(
+              context,
+              equalizerEnabled,
+              parameters: parameters,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                for (var band in parameters?.bands ?? List.filled(5, null))
+                  Expanded(
+                    child: Text('${band?.centerFrequency.round() ?? "--"} Hz'),
+                  ),
+              ],
+            ),
+          ]);
         },
       ),
     );
   }
 
+  Widget buildEqualizerControls(
+    BuildContext context,
+    bool equalizerEnabled, {
+    AndroidEqualizerParameters? parameters,
+  }) {
+    return CustomPaint(
+      painter: EqualizerOverlayPainter(
+        parameters: parameters,
+        lineColor: Theme.of(context).colorScheme.primary,
+        fillColor: Theme.of(context).colorScheme.primary,
+      ),
+      child: buildEqualizerSliders(context, equalizerEnabled,
+          parameters: parameters),
+    );
+  }
+
   Widget buildEqualizerSliders(
+    BuildContext context,
     bool equalizerEnabled, {
     AndroidEqualizerParameters? parameters,
   }) {
     return SizedBox(
       height: 250,
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          for (var band in parameters?.bands ?? List.filled(5, null))
-            Expanded(
-              child: Column(
-                children: [
-                  Expanded(
-                    child: StreamBuilder<double>(
-                      stream: band?.gainStream,
-                      builder: (context, snapshot) {
-                        return RotatedBox(
-                          quarterTurns: -1,
-                          child: Slider(
-                            min: parameters?.minDecibels ?? 0,
-                            max: parameters?.maxDecibels ?? 1,
-                            value: band?.gain ?? 0.5,
-                            onChanged: (!equalizerEnabled)
+      child: SliderTheme(
+        data: Theme.of(context).sliderTheme.copyWith(),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            for (var band in parameters?.bands ?? List.filled(5, null))
+              Expanded(
+                child: StreamBuilder<double>(
+                  stream: band?.gainStream,
+                  builder: (context, snapshot) {
+                    return RotatedBox(
+                      quarterTurns: -1,
+                      child: Slider(
+                        min: parameters?.minDecibels ?? 0,
+                        max: parameters?.maxDecibels ?? 1,
+                        value: band?.gain ?? 0.5,
+                        onChanged: (!equalizerEnabled)
+                            ? null
+                            : (parameters == null)
                                 ? null
-                                : (parameters == null)
-                                    ? null
-                                    : band?.setGain,
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  Text('${band?.centerFrequency.round() ?? "--"} Hz'),
-                ],
+                                : band?.setGain,
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/music_player/equalizer/equalizer_overlay.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+class EqualizerOverlayPainter extends CustomPainter {
+  EqualizerOverlayPainter({
+    required this.parameters,
+    required this.lineColor,
+    required this.fillColor,
+    this.lineWidth = 6.0,
+  });
+
+  final AndroidEqualizerParameters? parameters;
+
+  final Color lineColor;
+  final Color fillColor;
+
+  final double lineWidth;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (parameters == null) return;
+
+    /// How much the [Slider]'s track is padded
+    const double sliderTrackPadding = 24;
+
+    /// The actual height of the [Slider]'s track (and thus the maximum height of the overlay)
+    final double actualHeight = size.height - sliderTrackPadding * 2;
+
+    // Paints
+    final Paint linePaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = lineWidth
+      ..color = lineColor;
+    final Paint fillPaint = Paint()..style = PaintingStyle.fill;
+
+    // Generate spline points
+    List<Offset> controlPoints = List.generate(
+      parameters!.bands.length,
+      (index) {
+        AndroidEqualizerBand band = parameters!.bands[index];
+        double decibelFraction = (band.gain - parameters!.minDecibels) /
+            (parameters!.maxDecibels - parameters!.minDecibels);
+
+        return Offset(size.width * (index + 0.5) / parameters!.bands.length,
+            sliderTrackPadding + actualHeight * (1 - decibelFraction));
+      },
+    );
+
+    final CatmullRomSpline spline = CatmullRomSpline(controlPoints);
+    final List<Offset> splinePoints =
+        spline.generateSamples(tolerance: 1).map((e) => e.value).toList();
+    final Path splinePath = Path()..addPolygon(splinePoints, false);
+
+    // Fill
+    Shader fillShader({Alignment? start, Alignment? end}) => LinearGradient(
+          begin: start ?? Alignment.center,
+          end: end ?? Alignment.bottomCenter,
+          stops: const [0, 0.2, 1],
+          colors: [
+            fillColor.withOpacity(0),
+            fillColor.withOpacity(0.3),
+            fillColor.withOpacity(1),
+          ],
+        ).createShader(Offset.zero & size);
+
+    Path fillPath = Path()
+      ..addPolygon(splinePoints, false)
+      ..lineTo(
+          size.width * (1 - 0.5 / parameters!.bands.length), size.height / 2)
+      ..lineTo(size.width * 0.5 / parameters!.bands.length, size.height / 2);
+
+    // Draw fill below line
+    fillPaint.shader = fillShader(end: Alignment.bottomCenter);
+    canvas.drawPath(fillPath, fillPaint);
+    // Draw fill above line
+    fillPaint.shader = fillShader(end: Alignment.topCenter);
+    canvas.drawPath(fillPath, fillPaint);
+
+    // Draw line
+    canvas.drawPath(splinePath, linePaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant EqualizerOverlayPainter oldDelegate) {
+    print(parameters != oldDelegate.parameters);
+
+    return parameters != oldDelegate.parameters;
+  }
+}
