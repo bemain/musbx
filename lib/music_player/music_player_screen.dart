01d4a5359e47b7a339a9f4041802e8c638aa6352
BeMain <bemain.dev@gmail.com>
2023-03-19 22:11:17 +0100

    Fix MusicPlayer not rebuilding on state change

--- lib/music_player/music_player_screen.dart
+++ lib/music_player/music_player_screen.dart
@@ -26,48 +26,49 @@ class MusicPlayerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> tabs = [
-      CardList(
-        children: [
-          PitchSpeedCard(),
-          LoopCard(),
-        ],
-      ),
-      if (!Platform.isIOS)
-        CardList(
-          children: [EqualizerCard()],
-        ),
-    ];
-
     return ValueListenableBuilder(
-      valueListenable: MusicPlayer.instance.stateNotifier,
-      builder: (context, state, _) => Scaffold(
-        appBar: const DefaultAppBar(
-          helpText: """Load song from device or YouTube.
+        valueListenable: MusicPlayer.instance.stateNotifier,
+        builder: (context, state, _) {
+          List<Widget> tabs = [
+            CardList(
+              children: [
+                PitchSpeedCard(),
+                LoopCard(),
+              ],
+            ),
+            if (!Platform.isIOS)
+              CardList(
+                children: [EqualizerCard()],
+              ),
+          ];
+
+          return Scaffold(
+            appBar: const DefaultAppBar(
+              helpText: """Load song from device or YouTube.
 Adjust pitch and speed using the circular sliders. While selecting, greater accuracy can be obtained by dragging away from the center.
 If looping is enabled, change what section to loop using the range slider. Use the arrows to set the start or end of the section to the current position.""",
-        ),
-        body: DefaultTabController(
-          length: tabs.length,
-          child: Column(
-            children: [
-              if (tabs.length > 1) const EmptyTabBar(),
-              WidgetCard(
-                child: Column(
-                  children: [
-                    CurrentSongPanel(),
-                    PositionSlider(),
-                    ButtonPanel(),
-                  ],
-                ),
+            ),
+            body: DefaultTabController(
+              length: tabs.length,
+              child: Column(
+                children: [
+                  if (tabs.length > 1) const EmptyTabBar(),
+                  WidgetCard(
+                    child: Column(
+                      children: [
+                        CurrentSongPanel(),
+                        PositionSlider(),
+                        ButtonPanel(),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: TabBarView(children: tabs),
+                  ),
+                ],
               ),
-              Expanded(
-                child: TabBarView(children: tabs),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
+        });
   }
 }
