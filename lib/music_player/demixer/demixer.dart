f6cff82a4f4cba9921bacc719a4c6ad86122a6aa
BeMain <bemain.dev@gmail.com>
2023-06-27 11:04:48 +0200

    Adjust the minimum allowed position error

--- lib/music_player/demixer/demixer.dart
+++ lib/music_player/demixer/demixer.dart
@@ -138,7 +138,7 @@ class Demixer extends MusicPlayerComponent {
     MusicPlayer musicPlayer = MusicPlayer.instance;
     if (!isReady || !musicPlayer.isPlaying) return;
 
-    final Duration minAllowedPositionError = const Duration(milliseconds: 50) *
+    final Duration minAllowedPositionError = const Duration(milliseconds: 30) *
         (musicPlayer.slowdowner.enabled ? musicPlayer.slowdowner.speed : 1);
 
     for (Stem stem in stems) {
