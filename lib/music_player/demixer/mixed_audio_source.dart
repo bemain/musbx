8e939b110b512eb4f70fe4db797205b80590044c
BeMain <bemain.dev@gmail.com>
2023-09-20 14:31:04 +1200

    Respect volume of stems

--- lib/music_player/demixer/mixed_audio_source.dart
+++ lib/music_player/demixer/mixed_audio_source.dart
@@ -3,6 +3,17 @@ import 'dart:typed_data';
 
 import 'package:async/async.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:musbx/music_player/demixer/demixer.dart';
+import 'package:musbx/music_player/demixer/host.dart';
+import 'package:musbx/music_player/demixer/stem.dart';
+import 'package:musbx/music_player/music_player.dart';
+
+class StemFileData {
+  StemFileData({required this.stemType, required this.fileData});
+
+  final StemType stemType;
+  final List<int> fileData;
+}
 
 class MixedAudioSource extends StreamAudioSource {
   /// An [AudioSource] that mixes multiple .wav files.
@@ -10,16 +21,19 @@ class MixedAudioSource extends StreamAudioSource {
   /// All [files] must have the same duration
   MixedAudioSource(this.files) : super(tag: 'MixedAudioSource');
 
-  final List<File> files;
+  final Map<StemType, File> files;
 
   @override
   Future<StreamAudioResponse> request([int? start, int? end]) async {
-    int sourceLength = await files.first.length();
+    int sourceLength = await files.values.first.length();
 
-    Iterable<Stream<List<int>>> readStreams =
-        files.map((file) => file.openRead(start, end));
+    List<Stream<StemFileData>> readStreams = files.entries
+        .map((entry) => entry.value
+            .openRead(start, end)
+            .map((data) => StemFileData(stemType: entry.key, fileData: data)))
+        .toList();
     Stream<List<int>> mixed =
-        StreamZip<List<int>>(readStreams).map(mixWavFiles);
+        StreamZip<StemFileData>(readStreams).map(mixWavFiles);
 
     return StreamAudioResponse(
       sourceLength: sourceLength,
@@ -41,26 +55,40 @@ int fold(int x, int bits) => (x + (1 << (bits - 1))) % (1 << bits);
 /// 16 bits per sample is assumed.
 /// TODO: Check header for audio format (16, 32 bit...)
 ///
-/// The wav headers (if present) are (for simplicity) also mixed, which will cause undefined
-/// behaviour if the files have different headers.
-List<int> mixWavFiles(List<List<int>> dataLists) {
-  /// Convert byte lists from 8 bit to 16 bit.
-  List<Uint16List> uint16lists = [
-    for (final data in dataLists) Uint8List.fromList(data).buffer.asUint16List()
-  ];
+/// The wav headers (if present) are (for simplicity) also mixed, which will
+/// cause undefined behaviour if the files have different headers.
+List<int> mixWavFiles(List<StemFileData> dataLists) {
+  Demixer demixer = MusicPlayer.instance.demixer;
+  List<Stem> enabledStems =
+      demixer.stems.where((stem) => stem.enabled).toList();
 
-  /// Shift all values to make them signed (between `[-32768, 32767]` instead of `[0, 65536]`).
-  List<List<int>> shiftedLists = [
-    for (final uint16list in uint16lists)
-      [for (var i = 0; i < uint16list.length; i++) fold(uint16list[i], 16)]
-  ];
+  double totalVolume = 0;
+  List<List<double>> listsToMix = [];
+  for (Stem stem in enabledStems) {
+    StemFileData stemFileData = dataLists
+        .where((stemFileData) => stemFileData.stemType == stem.type)
+        .first;
 
-  // TODO: Consider volume of each wav
+    /// Convert byte lists from 8 bit to 16 bit.
+    Uint16List uint16list =
+        Uint8List.fromList(stemFileData.fileData).buffer.asUint16List();
+
+    List<double> processedList = [
+      for (var i = 0; i < uint16list.length; i++)
+        fold(uint16list[i], 16) // Shift all values to between `[-32768, 32767]`
+            *
+            stem.volume // Apply volume
+    ];
+
+    totalVolume += stem.volume;
+
+    listsToMix.add(processedList);
+  }
 
   /// Mix all byte lists into one.
   List<int> mixed = [
-    for (int i = 0; i < shiftedLists.first.length; i++)
-      shiftedLists.fold(0.0, (sum, shiftedList) => sum + shiftedList[i]) ~/ 4
+    for (int i = 0; i < listsToMix.first.length; i++)
+      (listsToMix.fold(0.0, (sum, list) => sum + list[i]) / totalVolume).round()
   ];
 
   /// Shift all values back to between `[0, 65536]`.
