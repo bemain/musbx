6f9fe8f9c1ecbac7defa760aa2747a20e07fc31d
BeMain <bemain.dev@gmail.com>
2023-09-21 20:15:17 +1200

    Detect wav header and avoid mixing it

--- lib/music_player/demixer/mixed_audio_source.dart
+++ lib/music_player/demixer/mixed_audio_source.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:async/async.dart';
+import 'package:flutter/foundation.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:musbx/music_player/demixer/demixer.dart';
 import 'package:musbx/music_player/demixer/host.dart';
@@ -43,61 +44,71 @@ class MixedAudioSource extends StreamAudioSource {
       contentType: 'audio/wav',
     );
   }
+
+  /// Mix multiple `wav` files together by taking the average value of each byte.
+  ///
+  /// 16 bits per sample and the files having identical headers is assumed.
+  /// TODO: Check header for audio format (16, 32 bit...)
+  List<int> mixWavFiles(List<StemFileData> dataLists) {
+    Demixer demixer = MusicPlayer.instance.demixer;
+    List<Stem> enabledStems =
+        demixer.stems.where((stem) => stem.enabled).toList();
+
+    // Try to detect wav header
+    // This method isn't fool proof (might by accident be these bytes at the start of the audio sample...) but it works for now
+    bool headerPresent = dataLists.every((stemFileData) =>
+        listEquals(
+          stemFileData.fileData.sublist(0, 4), [82, 73, 70, 70], // RIFF
+        ) &&
+        listEquals(stemFileData.fileData.sublist(8, 16),
+            [87, 65, 86, 69, 102, 109, 116, 32] // WAVEfmt
+            ));
+
+    List<List<double>> listsToMix = [];
+    for (Stem stem in enabledStems) {
+      StemFileData stemFileData = dataLists
+          .where((stemFileData) => stemFileData.stemType == stem.type)
+          .first;
+
+      List<int> data = headerPresent
+          ? stemFileData.fileData.sublist(44) // Remove header
+          : stemFileData.fileData;
+
+      // Convert byte lists from 8 bit to 16 bit
+      Uint16List uint16list = Uint8List.fromList(data).buffer.asUint16List();
+
+      List<double> processedList = [
+        for (var i = 0; i < uint16list.length; i++)
+          // Shift all values to between `[-32768, 32767]`
+          (fold(uint16list[i], 16) - 32768) * stem.volume // Apply volume
+      ];
+      listsToMix.add(processedList);
+    }
+
+    // Mix all byte lists into one
+    List<int> mixed = [
+      for (int i = 0; i < listsToMix.first.length; i++)
+        (listsToMix.fold(0.0, (sum, list) => sum + list[i]) / 4).round()
+    ];
+
+    // Shift all values back to between `[0, 65536]`
+    List<int> unshifted = [
+      for (var i = 0; i < mixed.length; i++) (fold(mixed[i], 16) + 32768)
+    ];
+
+    // Convert back to 8 bit
+    Uint8List uint8list = Uint16List.fromList(unshifted).buffer.asUint8List();
+
+    if (headerPresent) {
+      // Return header and then data
+      return dataLists.first.fileData.sublist(0, 44) + uint8list;
+    }
+
+    return uint8list;
+  }
 }
 
 /// Shifts int [x] of bit width [bits] up by half the total range, then wraps
 /// any overflowing values around to maintain the bit width. This is used to
 /// convert between signed and unsigned PCM.
 int fold(int x, int bits) => (x + (1 << (bits - 1))) % (1 << bits);
-
-/// Mix multiple `wav` files together by taking the average value of each byte.
-///
-/// 16 bits per sample is assumed.
-/// TODO: Check header for audio format (16, 32 bit...)
-///
-/// The wav headers (if present) are (for simplicity) also mixed, which will
-/// cause undefined behaviour if the files have different headers.
-List<int> mixWavFiles(List<StemFileData> dataLists) {
-  Demixer demixer = MusicPlayer.instance.demixer;
-  List<Stem> enabledStems =
-      demixer.stems.where((stem) => stem.enabled).toList();
-
-  double totalVolume = 0;
-  List<List<double>> listsToMix = [];
-  for (Stem stem in enabledStems) {
-    StemFileData stemFileData = dataLists
-        .where((stemFileData) => stemFileData.stemType == stem.type)
-        .first;
-
-    /// Convert byte lists from 8 bit to 16 bit.
-    Uint16List uint16list =
-        Uint8List.fromList(stemFileData.fileData).buffer.asUint16List();
-
-    List<double> processedList = [
-      for (var i = 0; i < uint16list.length; i++)
-        fold(uint16list[i], 16) // Shift all values to between `[-32768, 32767]`
-            *
-            stem.volume // Apply volume
-    ];
-
-    totalVolume += stem.volume;
-
-    listsToMix.add(processedList);
-  }
-
-  /// Mix all byte lists into one.
-  List<int> mixed = [
-    for (int i = 0; i < listsToMix.first.length; i++)
-      (listsToMix.fold(0.0, (sum, list) => sum + list[i]) / totalVolume).round()
-  ];
-
-  /// Shift all values back to between `[0, 65536]`.
-  List<int> unshifted = [
-    for (var i = 0; i < mixed.length; i++) fold(mixed[i], 16)
-  ];
-
-  /// Convert back to 8 bit.
-  Uint8List uint8list = Uint16List.fromList(unshifted).buffer.asUint8List();
-
-  return uint8list;
-}
