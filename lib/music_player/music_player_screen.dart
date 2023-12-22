17c0ce842c5baa8422d0ae2c1f44b3b376a6b01c
BeMain <bemain.dev@gmail.com>
2023-12-22 10:15:34 +0100

    feat(music-player): move position card to the bottom of the screen
    It is now much clearer that it is static and doesn't scroll with the
    rest of the content. This also reduces the need for tabs, so the
    remaining cards are simply arrayed in a list.

--- lib/music_player/music_player_screen.dart
+++ lib/music_player/music_player_screen.dart
@@ -12,13 +12,12 @@ import 'package:musbx/music_player/pick_song_button/components/search_youtube_bu
 import 'package:musbx/music_player/pick_song_button/components/upload_file_button.dart';
 import 'package:musbx/music_player/slowdowner/slowdowner_card.dart';
 import 'package:musbx/music_player/position_card/button_panel.dart';
-import 'package:musbx/music_player/current_song_card/current_song_panel.dart';
+import 'package:musbx/music_player/position_card/current_song_panel.dart';
 import 'package:musbx/music_player/position_card/position_slider.dart';
 import 'package:musbx/music_player/song.dart';
 import 'package:musbx/music_player/song_source.dart';
 import 'package:musbx/music_player/pick_song_button/speed_dial.dart';
 import 'package:musbx/music_player/pick_song_button/components/action.dart';
-import 'package:musbx/screen/card_list.dart';
 import 'package:musbx/screen/default_app_bar.dart';
 import 'package:musbx/screen/widget_card.dart';
 
@@ -76,73 +75,73 @@ class MusicPlayerScreenState extends State<MusicPlayerScreen>
     super.build(context);
 
     return ValueListenableBuilder(
-        valueListenable: musicPlayer.stateNotifier,
-        builder: (context, state, _) {
-          List<Widget> tabs = [
-            CardList(
-              children: [
-                SlowdownerCard(),
-                LoopCard(),
-              ],
-            ),
-            CardList(
-              children: [DemixerCard()],
-            ),
-            if (!Platform.isIOS)
-              CardList(
-                children: [EqualizerCard()],
-              ),
-          ];
+      valueListenable: musicPlayer.stateNotifier,
+      builder: (context, state, _) {
+        final List<Song> songHistory = musicPlayer.songHistory
+            .sorted(ascending: true)
+          ..remove(musicPlayer.song);
 
-          final List<Song> songHistory = musicPlayer.songHistory
-              .sorted(ascending: true)
-            ..remove(musicPlayer.song);
-
-          return Scaffold(
-            appBar: DefaultAppBar(
-              helpText: """Load song from device or YouTube.
+        return Scaffold(
+          appBar: DefaultAppBar(
+            helpText: """Load song from device or YouTube.
 The Slowdowner allows you to adjust pitch and speed using the circular sliders. While selecting, greater accuracy can be obtained by dragging away from the center.
 Change what section to loop using the range slider. Use the arrows to set the start or end of the section to the current position.
 Mute or isolate specific instruments using the Demixer.
 ${Platform.isAndroid ? "Use the Equalizer to adjust the gain of individual frequency bands." : ""}""",
-            ),
-            floatingActionButton: SpeedDial(
-              children: [
-                ...(songHistory).map(_buildHistoryItem).toList(),
-                if (songHistory.isNotEmpty) SpeedDialSpacer(),
-                SearchYoutubeButton(),
-                UploadSongButton(),
-              ],
-              expandedChild: const Icon(Icons.close_rounded),
-              child: const Icon(Icons.add_rounded),
-            ),
-            body: DefaultTabController(
-              length: tabs.length,
-              child: Column(
+          ),
+          floatingActionButton: SpeedDial(
+            children: [
+              ...(songHistory).map(_buildHistoryItem).toList(),
+              if (songHistory.isNotEmpty) SpeedDialSpacer(),
+              SearchYoutubeButton(),
+              UploadSongButton(),
+            ],
+            expandedChild: const Icon(Icons.close_rounded),
+            child: const Icon(Icons.add_rounded),
+          ),
+          body: Stack(
+            children: [
+              ListView(
                 children: [
-                  if (tabs.length > 1)
-                    TabBar(tabs: [
-                      const Tab(text: "Slowdowner"),
-                      const Tab(text: "Demixer"),
-                      if (!Platform.isIOS) const Tab(text: "Equalizer"),
-                    ]),
-                  WidgetCard(
-                    child: CurrentSongPanel(),
-                  ),
-                  WidgetCard(
-                    child: Column(children: [
-                      PositionSlider(),
-                      ButtonPanel(),
-                    ]),
-                  ),
-                  Expanded(
-                    child: TabBarView(children: tabs),
-                  ),
+                  WidgetCard(child: SlowdownerCard()),
+                  WidgetCard(child: LoopCard()),
+                  WidgetCard(child: DemixerCard()),
+                  WidgetCard(child: EqualizerCard()),
+                  const SizedBox(height: 212)
                 ],
               ),
-            ),
-          );
-        });
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(12.0)),
+                  ),
+                  color: Theme.of(context).colorScheme.surface,
+                  elevation: 3.0,
+                  margin: const EdgeInsets.only(top: 4.0),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 8,
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        CurrentSongPanel(),
+                        PositionSlider(),
+                        ButtonPanel(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   SpeedDialChild _buildHistoryItem(Song song) {
@@ -161,7 +160,6 @@ ${Platform.isAndroid ? "Use the Equalizer to adjust the gain of individual frequ
                       ? const YoutubeUnavailableDialog()
                       : const FileCouldNotBeLoadedDialog(),
                 );
-
                 // Restore state
                 musicPlayer.stateNotifier.value = prevState;
                 return;
