e312975ebdcf61f5b81674f87de58bd0f03ecd1e
BeMain <bemain.dev@gmail.com>
2024-01-30 12:49:49 +0100

    feat(music-player): create welcome screen
    Shown when no song i loaded

--- lib/music_player/music_player_screen.dart
+++ lib/music_player/music_player_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:musbx/custom_icons.dart';
 import 'package:musbx/music_player/demixer/demixer_card.dart';
 import 'package:musbx/music_player/equalizer/equalizer_card.dart';
@@ -44,6 +45,13 @@ class MusicPlayerScreen extends StatefulWidget {
 
 class MusicPlayerScreenState extends State<MusicPlayerScreen>
     with WidgetsBindingObserver, AutomaticKeepAliveClientMixin {
+  static const String helpText =
+      """Press the plus-button and load a song from your device or YouTube.
+
+- Adjust pitch and speed using the circular sliders. Greater accuracy can be obtained by dragging away from the center.
+- Loop a section of the song using the range slider. Use the arrows to set the start or end of the section to the current position.
+- Mute or isolate specific instruments using the Demixer.""";
+
   final MusicPlayer musicPlayer = MusicPlayer.instance;
 
   @override
@@ -91,7 +99,7 @@ class MusicPlayerScreenState extends State<MusicPlayerScreen>
               if (songHistory.isNotEmpty) SpeedDialSpacer(),
               UploadSongButton(),
             ],
-            onExpandedPressed: MusicPlayer.instance.isLoading
+            onExpandedPressed: musicPlayer.isLoading
                 ? null
                 : () async {
                     await pickYoutubeSong(context);
@@ -100,81 +108,124 @@ class MusicPlayerScreenState extends State<MusicPlayerScreen>
             expandedLabel: const Text("Search YouTube"),
             child: const Icon(Icons.add_rounded),
           ),
-          body: Stack(
-            children: [
-              ListView(
-                children: [
-                  const DefaultAppBar(
-                    scrolledUnderElevation: 0.0,
-                    helpText:
-                        """Press the plus-button and load a song from your device or YouTube.
-
-- Adjust pitch and speed using the circular sliders. Greater accuracy can be obtained by dragging away from the center.
-- Loop a section of the song using the range slider. Use the arrows to set the start or end of the section to the current position.
-- Mute or isolate specific instruments using the Demixer.""",
-                  ),
-                  WidgetCard(child: SlowdownerCard()),
-                  WidgetCard(child: LoopCard()),
-                  WidgetCard(child: DemixerCard()),
-                  if (!Platform.isIOS) WidgetCard(child: EqualizerCard()),
-                  if (positionCardSize != null)
-                    SizedBox(height: positionCardSize!.height + 4.0)
-                ],
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: MeasureSize(
-                  onSizeChanged: (Size size) {
-                    setState(() {
-                      positionCardSize = size;
-                    });
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Theme.of(context)
-                              .colorScheme
-                              .shadow
-                              .withOpacity(0.25),
-                          blurRadius: 8.0,
-                          spreadRadius: 4.0,
-                        )
+          body: state == MusicPlayerState.idle
+              ? _buildWelcomeScreen(context)
+              : Stack(
+                  children: [
+                    ListView(
+                      children: [
+                        const DefaultAppBar(
+                          scrolledUnderElevation: 0.0,
+                          helpText: helpText,
+                        ),
+                        WidgetCard(child: SlowdownerCard()),
+                        WidgetCard(child: LoopCard()),
+                        WidgetCard(child: DemixerCard()),
+                        if (!Platform.isIOS) WidgetCard(child: EqualizerCard()),
+                        if (positionCardSize != null)
+                          SizedBox(height: positionCardSize!.height + 4.0)
                       ],
                     ),
-                    child: Card(
-                      clipBehavior: Clip.antiAlias,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.vertical(
-                          top: Radius.circular(12.0),
-                        ),
-                      ),
-                      color: Theme.of(context).colorScheme.surface,
-                      elevation: 3.0,
-                      margin: EdgeInsets.zero,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 12,
-                          vertical: 8,
-                        ),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            CurrentSongPanel(),
-                            PositionSlider(),
-                            ButtonPanel(),
-                          ],
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: MeasureSize(
+                        onSizeChanged: (Size size) {
+                          setState(() {
+                            positionCardSize = size;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .shadow
+                                    .withOpacity(0.25),
+                                blurRadius: 8.0,
+                                spreadRadius: 4.0,
+                              )
+                            ],
+                          ),
+                          child: Card(
+                            clipBehavior: Clip.antiAlias,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(12.0),
+                              ),
+                            ),
+                            color: Theme.of(context).colorScheme.surface,
+                            elevation: 3.0,
+                            margin: EdgeInsets.zero,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 12,
+                                vertical: 8,
+                              ),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  CurrentSongPanel(),
+                                  PositionSlider(),
+                                  ButtonPanel(),
+                                ],
+                              ),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
-              ),
-            ],
-          ),
         );
       },
+    );
+  }
+
+  Widget _buildWelcomeScreen(BuildContext context) {
+    return Scaffold(
+      appBar: const DefaultAppBar(helpText: helpText),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              "Ready to",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Text(
+              "Transcribe",
+              style: GoogleFonts.zeyadaTextTheme(Theme.of(context).textTheme)
+                  .displayLarge,
+            ),
+            Text(
+              "using AI technology?",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 16.0),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Press the"),
+                Icon(Icons.add_rounded),
+                Text("to begin")
+              ],
+            ),
+            const SizedBox(height: 64.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 70),
+              child: Image.asset(
+                "assets/images/arrow.png",
+                width: 100,
+              ),
+            ),
+            const SizedBox(height: 64.0),
+          ],
+        ),
+      ),
     );
   }
 
