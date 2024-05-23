2f976e4f7801c48f0b3ca72c797967b825cb76cd
BeMain <bemain.dev@gmail.com>
2024-05-23 17:48:26 +0200

    feat(music-player): create host for the chords API

---
+++ lib/music_player/musbx_api/chords_api.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+
+import 'package:musbx/music_player/musbx_api/musbx_api.dart';
+
+class ChordsApiHost extends MusbxApiHost {
+  ChordsApiHost(super.address, {super.https});
+
+  Future analyzeFile(File file) async {
+    Uri url = uriConstructor(address, "/upload");
+    var request = http.MultipartRequest("POST", url);
+    request.headers.addAll({
+      ...MusbxApiHost.authHeaders,
+    });
+    request.files.add(await http.MultipartFile.fromPath(
+      "file",
+      file.path,
+      contentType: MediaType("audio", file.path.split('.').last),
+    ));
+
+    var response = await request.send();
+    Map<String, dynamic> json =
+        jsonDecode(await response.stream.bytesToString());
+
+    if (response.statusCode != 201) {
+      throw HttpException(json["message"], uri: url);
+    }
+
+    final chords = json["chords"];
+    return chords;
+  }
+
+  Future analyzeYoutubeSong(String youtubeId) async {
+    var response = await post("/analyze/$youtubeId");
+    Map<String, dynamic> json = jsonDecode(response.body);
+
+    if (response.statusCode != 201) {
+      throw HttpException(json["message"], uri: response.request?.url);
+    }
+
+    final chords = json["chords"];
+    return chords;
+  }
+}
