007b55a6dc7043a97eb6a92eb6d8f5ef16d5de92
BeMain <bemain.dev@gmail.com>
2024-06-04 14:03:15 +0200

    refactor(analyzer): extract `AnalyzerCard` widget

---
+++ lib/music_player/analyzer/analyzer_card.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:musbx/music_player/analyzer/chords_display.dart';
+import 'package:musbx/music_player/analyzer/waveform_widget.dart';
+import 'package:musbx/music_player/music_player.dart';
+
+class AnalyzerCard extends StatelessWidget {
+  AnalyzerCard({super.key});
+
+  final MusicPlayer musicPlayer = MusicPlayer.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 128,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const ChordsDisplay(),
+          WaveformWidget(),
+        ],
+      ),
+    );
+  }
+}
--- lib/music_player/looper/loop_card.dart
+++ lib/music_player/looper/loop_card.dart
@@ -1,8 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:musbx/music_player/analyzer/chords_display.dart';
-import 'package:musbx/music_player/analyzer/waveform_widget.dart';
+import 'package:musbx/music_player/analyzer/analyzer_card.dart';
 import 'package:musbx/music_player/looper/loop_slider.dart';
 import 'package:musbx/music_player/looper/looper.dart';
 import 'package:musbx/music_player/music_player.dart';
@@ -82,8 +81,7 @@ class LoopCard extends StatelessWidget {
         ],
       ),
       LoopSlider(),
-      const ChordsDisplay(),
-      WaveformWidget(),
+      AnalyzerCard(),
     ]);
   }
 
