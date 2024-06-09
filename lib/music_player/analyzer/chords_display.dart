1134e354b0da740f3756dd5540d4b4fbb2f30411
BeMain <bemain.dev@gmail.com>
2024-06-09 18:22:53 +0200

    fix(analyzer): remove position marker from `ChordsDisplay`

--- lib/music_player/analyzer/chords_display.dart
+++ lib/music_player/analyzer/chords_display.dart
@@ -15,49 +15,48 @@ class _ChordsDisplayState extends State<ChordsDisplay> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) => ValueListenableBuilder(
-        valueListenable: musicPlayer.analyzer.chordsNotifier,
-        builder: (context, chords, child) {
-          if (chords == null) {
-            return const Center(child: LinearProgressIndicator());
-          }
+    return SizedBox(
+      height: 24.0,
+      child: LayoutBuilder(
+        builder: (context, constraints) => ValueListenableBuilder(
+          valueListenable: musicPlayer.analyzer.chordsNotifier,
+          builder: (context, chords, child) {
+            if (chords == null) return const SizedBox();
 
-          return ValueListenableBuilder(
-            valueListenable: musicPlayer.analyzer.durationShownNotifier,
-            builder: (context, durationShown, child) => ValueListenableBuilder(
-              valueListenable: musicPlayer.positionNotifier,
-              builder: (context, position, child) {
-                Duration minDuration = position - durationShown * 0.5;
-                Duration maxDuration = position + durationShown * 0.5;
-                List<MapEntry<Duration, Chord?>> shownChords = chords.entries
-                    .where((e) => e.key > minDuration && e.key < maxDuration)
-                    .toList();
-                return Stack(
-                  alignment: Alignment.bottomCenter,
-                  children: [
-                    const Align(
-                      alignment: Alignment.center,
-                      child: Icon(Icons.circle, color: Colors.grey),
-                    ),
-                    ...shownChords.map((e) {
-                      final Chord? chord = e.value;
-                      return chord == null
-                          ? const SizedBox()
-                          : Positioned(
-                              left: ((e.key - position).inMilliseconds /
-                                          (durationShown.inMilliseconds) +
-                                      0.5) *
-                                  constraints.maxWidth,
-                              child: ChordSymbol(chord: chord),
-                            );
-                    }),
-                  ],
-                );
-              },
-            ),
-          );
-        },
+            return ValueListenableBuilder(
+              valueListenable: musicPlayer.analyzer.durationShownNotifier,
+              builder: (context, durationShown, child) =>
+                  ValueListenableBuilder(
+                valueListenable: musicPlayer.positionNotifier,
+                builder: (context, position, child) {
+                  Duration minDuration = position - durationShown * 0.5;
+                  Duration maxDuration = position + durationShown * 0.5;
+                  List<MapEntry<Duration, Chord?>> shownChords = chords.entries
+                      .where((e) => e.key > minDuration && e.key < maxDuration)
+                      .toList();
+
+                  return Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      ...shownChords.map((e) {
+                        final Chord? chord = e.value;
+                        return chord == null
+                            ? const SizedBox()
+                            : Positioned(
+                                left: ((e.key - position).inMilliseconds /
+                                            (durationShown.inMilliseconds) +
+                                        0.5) *
+                                    constraints.maxWidth,
+                                child: ChordSymbol(chord: chord),
+                              );
+                      }),
+                    ],
+                  );
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
