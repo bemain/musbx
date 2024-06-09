d169d8daa2fcdf8b7b43df9a70e387a5410b976e
BeMain <bemain.dev@gmail.com>
2024-06-09 18:10:13 +0200

    fix(analyzer): wrap `GestureDetector` around all Analyzer widgets

--- lib/music_player/analyzer/analyzer_card.dart
+++ lib/music_player/analyzer/analyzer_card.dart
@@ -8,17 +8,48 @@ class AnalyzerCard extends StatelessWidget {
 
   final MusicPlayer musicPlayer = MusicPlayer.instance;
 
+  /// Whether the MusicPlayer was playing before the user began dragging.
+  static bool wasPlayingBeforeChange = false;
+
+  /// The duration shown before the user began zooming.
+  static Duration durationShownBeforeChange = Duration.zero;
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 128,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const ChordsDisplay(),
-          Expanded(child: WaveformWidget()),
-        ],
+      child: GestureDetector(
+        onHorizontalDragStart: (_) {
+          wasPlayingBeforeChange = musicPlayer.isPlaying;
+          musicPlayer.pause();
+        },
+        onHorizontalDragUpdate: musicPlayer.nullIfNoSongElse((details) {
+          // TODO: Remove stuttering when dragging
+          if (details.delta.dx.abs() < 0.0) return;
+          musicPlayer.seek(musicPlayer.position -
+              musicPlayer.analyzer.durationShown * (details.delta.dx / 128));
+        }),
+        onHorizontalDragEnd: (_) {
+          if (wasPlayingBeforeChange) musicPlayer.play();
+        },
+        onHorizontalDragCancel: () {
+          if (wasPlayingBeforeChange) musicPlayer.play();
+        },
+        onScaleStart: (_) {
+          durationShownBeforeChange = musicPlayer.analyzer.durationShown;
+        },
+        onScaleUpdate: (details) {
+          musicPlayer.analyzer.durationShownNotifier.value =
+              durationShownBeforeChange * (1 / details.scale);
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const ChordsDisplay(),
+            Expanded(child: WaveformWidget()),
+          ],
+        ),
       ),
     );
   }
--- lib/music_player/analyzer/waveform_widget.dart
+++ lib/music_player/analyzer/waveform_widget.dart
@@ -7,10 +7,6 @@ class WaveformWidget extends StatelessWidget {
 
   final MusicPlayer musicPlayer = MusicPlayer.instance;
 
-  /// Whether the MusicPlayer was playing before the user began changing the position.
-  static bool wasPlayingBeforeChange = false;
-  static Duration durationShownBeforeChange = Duration.zero;
-
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
@@ -20,46 +16,21 @@ class WaveformWidget extends StatelessWidget {
           return const CircularProgressIndicator();
         }
 
-        return GestureDetector(
-          onHorizontalDragStart: (_) {
-            wasPlayingBeforeChange = musicPlayer.isPlaying;
-            musicPlayer.pause();
-          },
-          onHorizontalDragUpdate: musicPlayer.nullIfNoSongElse((details) {
-            // TODO: Remove stuttering when dragging
-            if (details.delta.dx.abs() < 0.0) return;
-            musicPlayer.seek(musicPlayer.position -
-                musicPlayer.analyzer.durationShown * (details.delta.dx / 128));
-          }),
-          onHorizontalDragEnd: (_) {
-            if (wasPlayingBeforeChange) musicPlayer.play();
-          },
-          onHorizontalDragCancel: () {
-            if (wasPlayingBeforeChange) musicPlayer.play();
-          },
-          onScaleStart: (_) {
-            durationShownBeforeChange = musicPlayer.analyzer.durationShown;
-          },
-          onScaleUpdate: (details) {
-            musicPlayer.analyzer.durationShownNotifier.value =
-                durationShownBeforeChange * (1 / details.scale);
-          },
-          child: ValueListenableBuilder(
-            valueListenable: musicPlayer.analyzer.durationShownNotifier,
-            builder: (context, duraionShown, child) => ValueListenableBuilder(
-              valueListenable: musicPlayer.positionNotifier,
-              builder: (context, position, child) {
-                return CustomPaint(
-                  painter: WaveformPainter(
-                    waveform: waveform,
-                    start: position - duraionShown * 0.5,
-                    duration: duraionShown,
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-                  size: const Size(double.infinity, 100.0),
-                );
-              },
-            ),
+        return ValueListenableBuilder(
+          valueListenable: musicPlayer.analyzer.durationShownNotifier,
+          builder: (context, duraionShown, child) => ValueListenableBuilder(
+            valueListenable: musicPlayer.positionNotifier,
+            builder: (context, position, child) {
+              return CustomPaint(
+                painter: WaveformPainter(
+                  waveform: waveform,
+                  start: position - duraionShown * 0.5,
+                  duration: duraionShown,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                size: const Size(double.infinity, 100.0),
+              );
+            },
           ),
         );
       },
