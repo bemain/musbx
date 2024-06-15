8495e2517c613d91d837c13fba4fa024200d1df4
BeMain <bemain.dev@gmail.com>
2024-06-15 13:25:15 +0200

    feat(ads): create bottom banner ad
    Ads are only shown on the 'free' flavor of the app

---
+++ lib/ads.dart
@@ -0,0 +1,92 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class BannerAdWidget extends StatefulWidget {
+  const BannerAdWidget({super.key});
+
+  @override
+  State<BannerAdWidget> createState() => _BannerAdWidgetState();
+}
+
+class _BannerAdWidgetState extends State<BannerAdWidget> {
+  /// The AdMob ad unit to show.
+  final String adUnitId = kDebugMode
+      // Test banner ad units, provided by Google
+      ? Platform.isAndroid
+          ? "ca-app-pub-3940256099942544/6300978111"
+          : "ca-app-pub-3940256099942544/2934735716"
+      // Real ad units
+      : Platform.isAndroid
+          ? "ca-app-pub-5107868608906815/7921762904"
+          : "ca-app-pub-5107868608906815/5487171252";
+
+  AdSize? _adSize;
+
+  /// The banner ad to show. This is `null` until the ad is actually loaded.
+  BannerAd? _bannerAd;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _loadAd();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_bannerAd == null) {
+      return SizedBox(
+        width: _adSize?.width.toDouble(),
+        height: _adSize?.height.toDouble() ?? 60,
+      );
+    }
+
+    return SizedBox(
+      width: _bannerAd!.size.width.toDouble(),
+      height: _bannerAd!.size.height.toDouble(),
+      child: AdWidget(ad: _bannerAd!),
+    );
+  }
+
+  /// Load a banner ad.
+  Future<void> _loadAd() async {
+    _adSize = await AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
+        MediaQuery.of(context).size.width.truncate());
+    if (_adSize == null) {
+      debugPrint('[ADS] Unable to get height of anchored banner.');
+      return;
+    }
+
+    final bannerAd = BannerAd(
+      size: _adSize!,
+      adUnitId: adUnitId,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          if (!mounted) {
+            ad.dispose();
+            return;
+          }
+          setState(() {
+            _bannerAd = ad as BannerAd;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          debugPrint('[ADS] BannerAd failed to load: $error');
+          ad.dispose();
+        },
+      ),
+    );
+
+    // Start loading.
+    bannerAd.load();
+  }
+}
