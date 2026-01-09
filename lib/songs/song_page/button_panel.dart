c7b188dbd144a14df42f94e0ce79d1e3f4089be1
bemain <bemain.dev@gmail.com>
2026-01-10 03:22:35 +0800

    feat(songs): change icons for forward and rewind buttons

--- lib/songs/song_page/button_panel.dart
+++ lib/songs/song_page/button_panel.dart
@@ -3,13 +3,27 @@ import 'package:material_plus/material_plus.dart';
 import 'package:material_symbols_icons/symbols.dart';
 import 'package:musbx/songs/player/song_player.dart';
 import 'package:musbx/songs/player/songs.dart';
-import 'package:musbx/widgets/widgets.dart';
 
 class ButtonPanel extends StatelessWidget {
   /// Panel including play/pause, forward and rewind buttons for controlling a [SongPlayer].
   ///
   /// If no song is loaded, all buttons are disabled.
   const ButtonPanel({super.key});
+
+  Widget _buildButton({
+    required void Function(SongPlayer player)? onPressed,
+    required Widget icon,
+  }) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: IconButton(
+        onPressed: Songs.player == null || onPressed == null
+            ? null
+            : () => onPressed(Songs.player!),
+        icon: icon,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,99 +37,93 @@ class ButtonPanel extends StatelessWidget {
       height: 64.0,
       child: ShimmerLoading(
         isLoading: player == null,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              onPressed: player == null
-                  ? null
-                  : () {
-                      player.seek(Duration.zero);
-                    },
-              disabledColor: disabledColor,
-              icon: const ExpandedIcon(Symbols.skip_previous_rounded),
-            ),
+        child: ButtonTheme(
+          disabledColor: disabledColor,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _buildButton(
+                onPressed: (player) {
+                  player.seek(Duration.zero);
+                },
+                icon: const Icon(Symbols.skip_previous),
+              ),
 
-            ContinuousButton(
-              onContinuousPress: player == null
-                  ? null
-                  : () {
-                      player.seek(
-                        player.position - const Duration(seconds: 1),
-                      );
-                    },
-              child: IconButton(
-                onPressed: player == null
+              ContinuousButton(
+                interval: Duration(milliseconds: 10),
+                onContinuousPress: player == null
                     ? null
                     : () {
                         player.seek(
-                          player.position - const Duration(seconds: 1),
+                          player.position - const Duration(milliseconds: 100),
                         );
                       },
-                disabledColor: disabledColor,
-                icon: const ExpandedIcon(Symbols.fast_rewind_rounded),
+                child: _buildButton(
+                  onPressed: (player) {
+                    player.seek(
+                      player.position - const Duration(seconds: 5),
+                    );
+                  },
+                  icon: const Icon(Symbols.replay_5),
+                ),
               ),
-            ),
 
-            ValueListenableBuilder<bool>(
-              valueListenable:
-                  player?.isPlayingNotifier ?? ValueNotifier(false),
-              builder: (_, isPlaying, _) {
-                return AspectRatio(
-                  aspectRatio: 1.0,
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: IconButton.filled(
-                      onPressed: player == null
-                          ? null
-                          : () {
-                              if (isPlaying) {
-                                player.pause();
-                              } else {
-                                player.resume();
-                              }
-                            },
-                      disabledColor: disabledColor,
-                      icon: ExpandedIcon(
-                        isPlaying
-                            ? Symbols.stop_rounded
-                            : Symbols.play_arrow_rounded,
-                        fill: 1,
+              ValueListenableBuilder<bool>(
+                valueListenable:
+                    player?.isPlayingNotifier ?? ValueNotifier(false),
+                builder: (_, isPlaying, _) {
+                  return AspectRatio(
+                    aspectRatio: 1.0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: IconButton.filled(
+                        onPressed: player == null
+                            ? null
+                            : () {
+                                if (isPlaying) {
+                                  player.pause();
+                                } else {
+                                  player.resume();
+                                }
+                              },
+                        icon: Icon(
+                          isPlaying ? Symbols.stop : Symbols.play_arrow,
+                          fill: 1,
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
-            ),
+                  );
+                },
+              ),
 
-            ContinuousButton(
-              onContinuousPress: player == null
-                  ? null
-                  : () {
-                      player.seek(
-                        player.position + const Duration(seconds: 1),
-                      );
-                    },
-              child: IconButton(
-                onPressed: player == null
+              ContinuousButton(
+                interval: Duration(milliseconds: 10),
+                onContinuousPress: player == null
                     ? null
                     : () {
                         player.seek(
-                          player.position + const Duration(seconds: 1),
+                          player.position + const Duration(milliseconds: 100),
                         );
                       },
-                disabledColor: disabledColor,
-                icon: const ExpandedIcon(Symbols.fast_forward_rounded),
+                child: _buildButton(
+                  onPressed: (player) {
+                    player.seek(
+                      player.position + const Duration(seconds: 10),
+                    );
+                  },
+                  icon: const Icon(Symbols.forward_10),
+                ),
               ),
-            ),
 
-            // Placeholder, only there to take space so the play button is centered
-            const IconButton(
-              onPressed: null,
-              icon: Icon(null),
-            ),
-          ],
+              // Placeholder, only there to take space so the play button is centered
+              _buildButton(
+                onPressed: null,
+                icon: Icon(null),
+              ),
+            ],
+          ),
         ),
       ),
     );
