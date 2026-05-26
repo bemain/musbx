9669007777b263d9a35e03a6fc320eaaaed5ef2d
bemain <bemain.dev@gmail.com>
2026-05-26 17:06:13 +0200

    refactor: format files

--- lib/widgets/custom_icons.dart
+++ lib/widgets/custom_icons.dart
@@ -5,25 +5,109 @@ import 'package:flutter/widgets.dart';
 class CustomIcons {
   CustomIcons._();
 
-  static const youtube = IconData(0xf101, fontFamily: 'CustomIcons', fontPackage: null);
-  static const waveform_triangle = IconData(0xf102, fontFamily: 'CustomIcons', fontPackage: null);
-  static const waveform_square = IconData(0xf103, fontFamily: 'CustomIcons', fontPackage: null);
-  static const waveform_sine = IconData(0xf104, fontFamily: 'CustomIcons', fontPackage: null);
-  static const waveform_sawtooth = IconData(0xf105, fontFamily: 'CustomIcons', fontPackage: null);
-  static const waveform = IconData(0xf106, fontFamily: 'CustomIcons', fontPackage: null);
-  static const tuning_fork_rounded = IconData(0xf107, fontFamily: 'CustomIcons', fontPackage: null);
-  static const tuning_fork = IconData(0xf108, fontFamily: 'CustomIcons', fontPackage: null);
-  static const trumpet = IconData(0xf109, fontFamily: 'CustomIcons', fontPackage: null);
-  static const snare = IconData(0xf10a, fontFamily: 'CustomIcons', fontPackage: null);
-  static const semiquavers_four = IconData(0xf10b, fontFamily: 'CustomIcons', fontPackage: null);
-  static const quavers_two = IconData(0xf10c, fontFamily: 'CustomIcons', fontPackage: null);
-  static const quavers_three = IconData(0xf10d, fontFamily: 'CustomIcons', fontPackage: null);
-  static const microphone = IconData(0xf10e, fontFamily: 'CustomIcons', fontPackage: null);
-  static const metronome = IconData(0xf10f, fontFamily: 'CustomIcons', fontPackage: null);
-  static const guitar_head = IconData(0xf110, fontFamily: 'CustomIcons', fontPackage: null);
-  static const googleplay = IconData(0xf111, fontFamily: 'CustomIcons', fontPackage: null);
-  static const crotchet = IconData(0xf112, fontFamily: 'CustomIcons', fontPackage: null);
-  static const bass_head = IconData(0xf113, fontFamily: 'CustomIcons', fontPackage: null);
-  static const appstore = IconData(0xf114, fontFamily: 'CustomIcons', fontPackage: null);
-  static const accidentals = IconData(0xf115, fontFamily: 'CustomIcons', fontPackage: null);
+  static const youtube = IconData(
+    0xf101,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const waveform_triangle = IconData(
+    0xf102,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const waveform_square = IconData(
+    0xf103,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const waveform_sine = IconData(
+    0xf104,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const waveform_sawtooth = IconData(
+    0xf105,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const waveform = IconData(
+    0xf106,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const tuning_fork_rounded = IconData(
+    0xf107,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const tuning_fork = IconData(
+    0xf108,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const trumpet = IconData(
+    0xf109,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const snare = IconData(
+    0xf10a,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const semiquavers_four = IconData(
+    0xf10b,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const quavers_two = IconData(
+    0xf10c,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const quavers_three = IconData(
+    0xf10d,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const microphone = IconData(
+    0xf10e,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const metronome = IconData(
+    0xf10f,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const guitar_head = IconData(
+    0xf110,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const googleplay = IconData(
+    0xf111,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const crotchet = IconData(
+    0xf112,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const bass_head = IconData(
+    0xf113,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const appstore = IconData(
+    0xf114,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
+  static const accidentals = IconData(
+    0xf115,
+    fontFamily: 'CustomIcons',
+    fontPackage: null,
+  );
 }
