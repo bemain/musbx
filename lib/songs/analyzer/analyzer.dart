68367ac26cebf3fda9745e1dc06d1424e09bae2a
bemain <bemain.dev@gmail.com>
2026-08-04 13:20:50 +0200

    docs: host store badges on GitHub

--- lib/songs/analyzer/analyzer.dart
+++ lib/songs/analyzer/analyzer.dart
@@ -55,7 +55,7 @@ class AnalyzerComponent extends SongPlayerComponent {
   /// The process extracting the waveform from the current song,
   /// or `null` if no song has been loaded.
   ///
-  /// TODO: Try reimplementing this using SoLoud or waveform_extractor.
+  /// TODO: Try reimplementing this using SoLoud or audio_waveforms.
   late final WaveformExtractionProcess waveformProcess =
       WaveformExtractionProcess(player.song)
         ..resultNotifier.addListener(() {
